6b1433956fc74abc77d60ef2d21e00a840ca5922
Yomab <mabire.yoan@gmail.com>
2019-04-15 16:20:00 +0200

    exercice 4 terminé

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $('#hauteur').click(function(){
 var height10 = $('#rectangle').css('height','+=10px');
+console.log($('#rectangle').css('height'));
+if (($('#rectangle').css('height'))=='100px'){
+  $('#rectangle').css('height','10px');
+}
+});
+$('#green').click(function(){
+var green = $('#rectangle').css('background-color','green');
+});
+$('#initColor').click(function(){
+var firstcolor = $('#rectangle').css('background-color','#1e89d6');
+});
+$('#hide').click(function(){
+var hiderect = $('#rectangle').hide();
+});
+$('#show').click(function(){
+var hiderect = $('#rectangle').show();
 });
